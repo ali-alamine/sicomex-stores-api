7c29696a9475b3913a42172c68477eaa00f93298
unknown <aly.alameen@gmail.com>
2020-05-22 15:36:38 +0300

    Check if bank_check is duplicated and return an warning message

--- app/models/checkModel.js
+++ app/models/checkModel.js
@@ -17,62 +17,86 @@ var Check =function(check){
 
 Check.addNewCheck = function (check_data,result){
     sql.beginTransaction(function(err){
-        var insert_check_data={};
-        if(check_data.is_for_sup){
-            insert_check_data={'store_id':check_data.store_id,'supplier_id':check_data.supplier_id,'check_number':check_data.check_number,'check_amount':check_data.check_amount,'check_date':check_data.check_date,'is_paid':check_data.is_paid,'invoice_ids':check_data.invoice_ids,'is_for_sup':check_data.is_for_sup};
-        }else{
-            insert_check_data={'store_id':check_data.store_id,'supplier_id':check_data.supplier_id,'check_description':check_data.check_description,'check_number':check_data.check_number,'check_amount':check_data.check_amount,'check_date':check_data.check_date,'is_paid':check_data.is_paid,'invoice_ids':check_data.invoice_ids,'is_for_sup':check_data.is_for_sup};
-        }
-        sql.query('INSERT INTO bank_check SET ?',insert_check_data, function(err,res){
+        sql.query('SELECT * FROM bank_check WHERE check_number = ' +check_data.check_number,function(err,res){
             if(err){
                 sql.rollback(function() {
                     throw err;
                 });
             }else{
-                if(check_data.is_for_sup){
-                    var check_id=res.insertId;
-                    var data ={'check_id':check_id,'invoice_ids':check_data.invoice_ids};
-                    invoiceModel.updateInvoiceCheckID(data,function(err,res){
+                if(res.length == 0 ){
+                    var insert_check_data={};
+                    if(check_data.is_for_sup){
+                        insert_check_data={'store_id':check_data.store_id,'supplier_id':check_data.supplier_id,'check_number':check_data.check_number,'check_amount':check_data.check_amount,'check_date':check_data.check_date,'is_paid':check_data.is_paid,'invoice_ids':check_data.invoice_ids,'is_for_sup':check_data.is_for_sup};
+                    }else{
+                        insert_check_data={'store_id':check_data.store_id,'supplier_id':check_data.supplier_id,'check_description':check_data.check_description,'check_number':check_data.check_number,'check_amount':check_data.check_amount,'check_date':check_data.check_date,'is_paid':check_data.is_paid,'invoice_ids':check_data.invoice_ids,'is_for_sup':check_data.is_for_sup};
+                    }
+                    sql.query('INSERT INTO bank_check SET ?',insert_check_data, function(err,res){
                         if(err){
                             sql.rollback(function() {
                                 throw err;
                             });
                         }else{
-                            if(check_data.is_paid){
-                                var new_supplier_amount=(parseInt(check_data.supplier_amount) - parseInt(check_data.check_amount));
-                                var update_supplier_amount = {'new_supplier_amount':new_supplier_amount,'supplier_id':check_data.supplier_id};
-                                supplierModel.updateAmount(update_supplier_amount,function(err,response){
+                            if(check_data.is_for_sup){
+                                var check_id=res.insertId;
+                                var data ={'check_id':check_id,'invoice_ids':check_data.invoice_ids};
+                                invoiceModel.updateInvoiceCheckID(data,function(err,res){
                                     if(err){
                                         sql.rollback(function() {
                                             throw err;
                                         });
                                     }else{
-                                        sql.commit(function(err) {
-                                            if (err) { 
-                                                sql.rollback(function() {
-                                                    throw err;
-                                                });
-                                            }
-                                            result(null,res);
-                                        });
+                                        if(check_data.is_paid){
+                                            var new_supplier_amount=(parseInt(check_data.supplier_amount) - parseInt(check_data.check_amount));
+                                            var update_supplier_amount = {'new_supplier_amount':new_supplier_amount,'supplier_id':check_data.supplier_id};
+                                            supplierModel.updateAmount(update_supplier_amount,function(err,response){
+                                                if(err){
+                                                    sql.rollback(function() {
+                                                        throw err;
+                                                    });
+                                                }else{
+                                                    sql.commit(function(err) {
+                                                        if (err) { 
+                                                            sql.rollback(function() {
+                                                                throw err;
+                                                            });
+                                                        }
+                                                        result(null,res);
+                                                    });
+                                                }
+                                            })
+                                        }else{
+                                            sql.commit(function(err) {
+                                                if (err) { 
+                                                    sql.rollback(function() {
+                                                        throw err;
+                                                    });
+                                                }else{
+                                                    result(null,res);
+                                                }
+                                            });
+                                        }
                                     }
                                 })
+                            }else{
+                                sql.commit(function(err) {
+                                    if (err) { 
+                                        sql.rollback(function() {
+                                            throw err;
+                                        });
+                                    }else{
+                                        result(null,res);
+                                    }
+                                });
                             }
                         }
-                    })
-                }else{
-                    sql.commit(function(err) {
-                        if (err) { 
-                            sql.rollback(function() {
-                                throw err;
-                            });
-                        }else{
-                            result(null,res);
-                        }
                     });
+
+                }else{
+                    result('DUPLICATE_CHECK_NUM',res);
                 }
             }
-        });
+        })
+
     })
 }
 Check.getChecks = function (result){
