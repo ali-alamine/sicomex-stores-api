c1235ee2f661ec1a512b72b0081d9bba5ab2578e
unknown <aly.alameen@gmail.com>
2020-06-14 00:24:05 +0300

    Add database connection to connect to prod database

--- app/models/db.js
+++ app/models/db.js
@@ -3,18 +3,28 @@
 var mysql = require('mysql');
 
 //local mysql db connection
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'sicomex-stores-new'
-});
+var is_prod = true;
+if(is_prod){
+  var connection = mysql.createConnection({
+      host     : 'sicomex-stores-new.czgv9lejumuk.us-east-1.rds.amazonaws.com',
+      user     : 'root',
+      password : 'sicomex_stores',
+      database : 'sicomex_stores_new'
+  });
+}else{
+  var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '',
+      database : 'sicomex-stores-new'
+  });
+}
 
 connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('connected as id ' + connection.threadId);
-  });
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 module.exports = connection;
