403e2329a1f0b36e8dff169c07c1b32700ded7bf
Ali Al Amine <ali@melrok.com>
2021-12-05 21:58:25 +0200

    test 2

--- app/models/db.js
+++ app/models/db.js
@@ -5,8 +5,8 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
       host     : 'localhost',// 'database-2.chrjj6sace1q.af-south-1.rds.amazonaws.com',
-      user     : 'petitem',
-      password : 'U<U[{Bc+C!3sdRn2{{2t}?#m',
+      user     : 'root',
+      password : '',//'sicomex-stores',//'91j3funfidiuayfihun243unf78J@',
       database : 'sicomex-stores-new'//'sicomex_stores_new'
 });
 
